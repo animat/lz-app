e35705f101e5506560810dba0392e4b8f5d344a4
Colin Angevine <info@linguazone.com>
2015-07-15 00:56:31 -0400

    Prepended a / to URLs now that kconf.js is dictating base paths

--- www/js/services.js
+++ www/js/services.js
@@ -31,7 +31,7 @@ angular.module('linguazone.services', [])
   var course = {info: {}, available_games: [], available_word_lists: [], available_posts: []};
   return {
     updateAll: function(courseId) {
-      return $http.get(K.baseUrl.api+"courses/"+courseId).then(function(response) {
+      return $http.get(K.baseUrl.api+"/courses/"+courseId).then(function(response) {
         course.info = response.data.course;
         course.available_games = response.data.games;
         course.available_word_lists = response.data.word_lists;
@@ -40,17 +40,17 @@ angular.module('linguazone.services', [])
       });
     },
     getGameInfo: function(agId) {
-      return $http.get(K.baseUrl.api+"available_games/"+agId).then(function(response) {
+      return $http.get(K.baseUrl.api+"/available_games/"+agId).then(function(response) {
         return response.data;
       });
     },
     getWordListInfo: function(awlId) {
-      return $http.get(K.baseUrl.api+"available_word_lists/"+awlId).then(function(response) {
+      return $http.get(K.baseUrl.api+"/available_word_lists/"+awlId).then(function(response) {
         return response.data;
       });
     },
     getPostInfo: function(apId) {
-      return $http.get(K.baseUrl.api+"available_posts/"+apId).then(function(response) {
+      return $http.get(K.baseUrl.api+"/available_posts/"+apId).then(function(response) {
         return response.data;
       })
     },
@@ -61,7 +61,7 @@ angular.module('linguazone.services', [])
 .factory('RecentItems', function($http) {
   return {
     getAll: function() {
-      return $http.get(K.baseUrl.api+"feed_items/student/10052").then(function(response) {
+      return $http.get(K.baseUrl.api+"/feed_items/student/10052").then(function(response) {
         return response.data;
       });
     }
@@ -71,12 +71,12 @@ angular.module('linguazone.services', [])
 .factory('States', function($http) {
   return {
     getAll: function() {
-      return $http.get(K.baseUrl.api+"states").then(function(response) {
+      return $http.get(K.baseUrl.api+"/states").then(function(response) {
         return response.data;
       });
     },
     show: function(id) {
-      return $http.get(K.baseUrl.api+"states/"+id).then(function(response) {
+      return $http.get(K.baseUrl.api+"/states/"+id).then(function(response) {
         return response.data;
       });
     }
@@ -86,7 +86,7 @@ angular.module('linguazone.services', [])
 .factory('Schools', function($http) {
   return {
     show: function(id) {
-      return $http.get(K.baseUrl.api+"schools/"+id).then(function(response) {
+      return $http.get(K.baseUrl.api+"/schools/"+id).then(function(response) {
         return response.data;
       })
     }
@@ -98,7 +98,7 @@ angular.module('linguazone.services', [])
   var user = { info: {}, registrations: [] };
   return {
     updateStudentInfo: function(sid) {
-      return $http.get(K.baseUrl.api+"students/"+sid).then(function(response) {
+      return $http.get(K.baseUrl.api+"/students/"+sid).then(function(response) {
         user.info = response.data.student_data.student;
         user.registrations = response.data.student_data.registrations;
         var lastReg = response.data.student_data.registrations[response.data.student_data.registrations.length-1];
@@ -107,7 +107,7 @@ angular.module('linguazone.services', [])
       })
     },
     createRegistration: function(cId) {
-      return $http.post(K.baseUrl.api+"course_registrations", {courseId: cId}).then(function(response) {
+      return $http.post(K.baseUrl.api+"/course_registrations", {courseId: cId}).then(function(response) {
         return response.data;
       });
     },
